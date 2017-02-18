892f63b51ec61dfbb93d1cbfdfdfd64ba3ba43f6
gabrielamelian <gabrielamelian@gmail.com>
2017-02-18 18:57:27 +1300

    Adds a gassian blur effect to one of the images with ImageFilter

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 import os
 
-# image1 = Image.open('./Images/cubes.png')
 images_location = './Images/'
 
-size_300 = (300, 300)
+# size_300 = (300, 300)
+#
+# for f in os.listdir(images_location):
+#     if f.endswith('.jpg'):
+#         i = Image.open(images_location + f)
+#         fn, fext = os.path.splitext(f)
+#
+#         i.thumbnail(size_300)
+#         i.save(images_location + '300/{}_300{}'.format(fn, fext))
 
-for f in os.listdir(images_location):
-    if f.endswith('.jpg'):
-        i = Image.open(images_location + f)
-        fn, fext = os.path.splitext(f)
 
-        i.thumbnail(size_300)
-        i.save(images_location + '300/{}_300{}'.format(fn, fext))
-
-
-# image1.save('./Images/cubes.jpg')
-# image1.show()
+image1 = Image.open(images_location + 'cubes.png')
+image1.filter(ImageFilter.GaussianBlur(15)).save(images_location + 'cubesedit.png')
