1d37054f1eaa9a1b1821e6dac8ec1a9b993a8a5c
gabrielamelian <gabrielamelian@gmail.com>
2017-02-18 18:26:57 +1300

    Adds os module to interact with files, creates for loop to get all jpg files

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from PIL import Image
+import os
 
-image1 = Image.open('./Images/cubes.png')
+# image1 = Image.open('./Images/cubes.png')
+imagesLocation = './Images/'
 
+for f in os.listdir(imagesLocation):
+    if f.endswith('.jpg'):
+        i = Image.open(imagesLocation + f)
+        fn, fext = os.path.splitext(f)
+        print fn
 # image1.save('./Images/cubes.jpg')
-image1.show()
+# image1.show()
